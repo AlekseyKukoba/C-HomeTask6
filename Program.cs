b4e2e83f9ef3acd7ec409a1cc3e254e56680ef34
Aleksey <kukoba.alexey@gmail.com>
2023-07-11 15:57:10 +0300

    final

--- Program.cs
+++ Program.cs
@@ -2,3 +2,13 @@
 // 4->да
 // 3->нет 
 // 7->нет
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.Read()!);
+if (number % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
